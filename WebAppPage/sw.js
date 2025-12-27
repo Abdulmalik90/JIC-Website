f46d5a34de3f4ec7330c76c79c643266fc4ae431
Raed AlZahrani <rudyzhrani@gmail.com>
2025-12-27 10:28:23 +0300

    Update sw.js

--- WebAppPage/sw.js
+++ WebAppPage/sw.js
@@ -1,50 +1,56 @@
-const CACHE_NAME = 'madkhal-app-v8-local';
+const CACHE_NAME = 'madkhal-app-v9';
+
 const ASSETS_TO_CACHE = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './tools.html',
-    './toolstyle.css',
-    './toolsjava.js',
-    './library.html',
-    './libstyle.css',
-    './libscript.js',
-    './news.html',
-    './news-style.css',
-    './news-script.js',
-    './logo.png',
-    './Images/person.png',
-    'https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700;800&display=swap',
-    'https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'
+  '/WebAppPage/',
+  '/WebAppPage/index.html',
+
+  '/WebAppPage/style.css',
+  '/WebAppPage/script.js',
+
+  '/WebAppPage/tools.html',
+  '/WebAppPage/toolstyle.css',
+  '/WebAppPage/toolsjava.js',
+
+  '/WebAppPage/library.html',
+  '/WebAppPage/libstyle.css',
+  '/WebAppPage/libscript.js',
+
+  '/WebAppPage/news.html',
+  '/WebAppPage/news-style.css',
+  '/WebAppPage/news-script.js',
+
+  '/WebAppPage/logo.png',
+  '/WebAppPage/Images/person.png'
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
 });
