d013d88f75d380b23d5948bb7c2efbdecf08c316
Raed AlZahrani <rudyzhrani@gmail.com>
2025-12-28 07:01:36 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "منصة مدخل",
+  "short_name": "مدخل",
+  "start_url": "/WebAppPage/",
+  "scope": "/WebAppPage/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#0050d5",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "./web-app-manifest-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "./web-app-manifest-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
