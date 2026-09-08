93211e834bf8c18c4546e1bd6bd99ede5e20a1e2
Raed AlZahrani <rudyzhrani@gmail.com>
2026-09-08 19:18:03 +0300

    Add files via upload

--- WebAppPage/Tools/toolsjava.js
+++ WebAppPage/Tools/toolsjava.js
@@ -40,7 +40,7 @@ if (resultBtn) {
         }
 
         // --- الحسابات الأساسية ---
-        let totalClassesSemester = classPerWeek * 15; 
+        let totalClassesSemester = classPerWeek * 16; 
         let exactDNThreshold = Number((totalClassesSemester * 0.20).toFixed(1)); 
         
         let safeAllowedAbs = Math.floor(exactDNThreshold);
